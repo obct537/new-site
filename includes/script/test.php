6f6bf7c9955671fbfb398173db0029f51a2336f3
Sam Schwartz <obct537.gmail.com>
2012-02-03 19:31:53 -0500

    Removed some ancient experimental crap

--- includes/script/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if($output = exec('/bin/bash test.sh')){ 
-	echo "WINCAKE";
-}else{
-	echo "FAILCAKE";
-}
-echo $output;
-?>
--- includes/script/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-kill `ps -x | grep java`
-echo $?
